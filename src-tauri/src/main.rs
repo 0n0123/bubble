ba088dfe28126d28a3e01a1f5d834655ecb0e35b
ONO <dot.izumi@gmail.com>
2024-10-08 16:09:52 +0900

    エラーメッセージ対応他

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,48 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{collections::HashSet, sync::OnceLock};
+use std::sync::OnceLock;
 
-use anyhow::{anyhow, bail, Result};
-use cli::Args;
+use anyhow::{anyhow, Result};
 use flume::Receiver;
 use serde::{Deserialize, Serialize};
 use tauri::{AppHandle, Manager};
 use zenoh::{prelude::sync::*, publication::Publisher, subscriber::Subscriber};
 
-mod cli;
-mod conf;
 
 static SESSION: OnceLock<Session> = OnceLock::new();
 static PUBLISHER: OnceLock<Publisher> = OnceLock::new();
 static APP: OnceLock<AppHandle> = OnceLock::new();
 
 #[tauri::command]
-fn hello() {
-    let app_handle = APP.get();
-    let session = SESSION.get();
-    if let (Some(app_handle), Some(session)) = (app_handle, session) {
-        if let Ok(replies) = session.get("bubble/rooms").res() {
-            let mut room_ids = HashSet::new();
-            while let Ok(reply) = replies.recv() {
-                for c in reply.sample.unwrap().payload.slices() {
-                    if let Ok(id) = String::from_utf8(c.to_vec()) {
-                        room_ids.insert(id);
-                    }
-                }
-                dbg!("Room IDs", &room_ids);
-            }
-            let _ = app_handle.emit_all("rooms", room_ids);
-        }
+fn enter_room(server: &str, room: &str) {
+    if let Err(_) = prepare_session(server) {
+        eprintln!("Cannot connect to server {server}");
+        return;
     }
-}
 
-#[tauri::command]
-fn enter_room(room: &str) {
     let session = match SESSION.get() {
         Some(s) => s,
         None => {
-            dbg!("Session is illegal state.");
+            notify(Notice::new("Cannot connect to server."));
+            eprintln!("Session is illegal state.");
             return;
         }
     };
@@ -52,7 +35,8 @@ fn enter_room(room: &str) {
     match session.declare_subscriber(key.clone()).res() {
         Ok(s) => tauri::async_runtime::spawn(listen_message(s)),
         Err(_) => {
-            dbg!("Failed to declare subscriber", key);
+            notify(Notice::new("Cannot connect to server."));
+            eprintln!("Failed to declare subscriber {key}");
             return;
         }
     };
@@ -60,31 +44,13 @@ fn enter_room(room: &str) {
     match session.declare_publisher(key.clone()).res() {
         Ok(p) => PUBLISHER.set(p).expect("Failed to fix publisher."),
         Err(_) => {
-            dbg!("Failed to declare publisher", key);
+            notify(Notice::new("Cannot connect to server."));
+            eprintln!("Failed to declare publisher {key}");
             return;
         }
     };
 
-    let room_query = match session.declare_queryable("bubble/rooms").res() {
-        Ok(q) => q,
-        Err(_) => {
-            dbg!("Failed to declare queryable.");
-            return;
-        }
-    };
-    let room_id = room.to_owned();
-    tauri::async_runtime::spawn(async move {
-        while let Ok(query) = room_query.recv() {
-            query
-                .reply(Ok(
-                    Sample::try_from("bubble/rooms", room_id.to_owned()).unwrap()
-                ))
-                .res()
-                .unwrap();
-        }
-    });
-
-    dbg!("Enter:", room);
+    eprintln!("Enter: {room}");
 }
 
 #[tauri::command]
@@ -92,7 +58,8 @@ fn send_message(name: &str, message: &str) {
     let publisher = match PUBLISHER.get() {
         Some(p) => p,
         None => {
-            dbg!("Failed to get publisher.");
+            notify(Notice::new("Cannot connect to server."));
+            eprintln!("Failed to get publisher.");
             return;
         }
     };
@@ -103,7 +70,8 @@ fn send_message(name: &str, message: &str) {
     };
     let data = rmp_serde::to_vec(&mes).unwrap();
     if let Err(_) = publisher.put(data).res() {
-        dbg!("Failed to send message", mes);
+        notify(Notice::new("Cannot connect to server."));
+        eprintln!("Failed to send message");
     }
 }
 
@@ -112,7 +80,6 @@ async fn listen_message(subscriber: Subscriber<'_, Receiver<Sample>>) {
         let read = sample.payload.reader();
         if let Ok(mes) = rmp_serde::from_read::<_, Message>(read) {
             if let Some(app) = APP.get() {
-                dbg!("send message", &mes);
                 app.emit_all("message", mes)
                     .expect("Failed to emit message.");
             }
@@ -127,14 +94,6 @@ struct Message {
 }
 
 fn main() -> Result<()> {
-    let args = Args::load();
-    if !args.config.exists() {
-        bail!("Failed to read config.")
-    }
-
-    let config = conf::Config::read(&args.config)?;
-    prepare_session(config.server)?;
-
     tauri::Builder::default()
         .setup(|app| {
             let app_handle = app.app_handle();
@@ -142,17 +101,14 @@ fn main() -> Result<()> {
                 .map_err(|_| anyhow!("Failed to initialize application."))?;
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![hello, enter_room, send_message])
+        .invoke_handler(tauri::generate_handler![enter_room, send_message])
         .run(tauri::generate_context!())
         .map_err(|_| anyhow!("error while running tauri application"))
 }
 
-fn prepare_session(servers: Vec<String>) -> Result<()> {
-    let endpoints = servers
-        .iter()
-        .map(|server| EndPoint::new("tcp", server, "", ""))
-        .filter_map(|r| r.ok());
-    let config = zenoh::config::client(endpoints);
+fn prepare_session(server: &str) -> Result<()> {
+    let endpoint = EndPoint::new("tcp", server, "", "").map_err(|_| anyhow!("Failed to setup server config."))?;
+    let config = zenoh::config::client(vec![endpoint]);
     let session = zenoh::open(config)
         .res()
         .map_err(|_| anyhow!("Failed to open zenoh session."))?;
@@ -162,4 +118,24 @@ fn prepare_session(servers: Vec<String>) -> Result<()> {
         .map_err(|_| anyhow!("Failed to fix zenoh session."))?;
 
     Ok(())
+}
+
+#[derive(Serialize, Clone)]
+struct Notice {
+    message: String,
+}
+
+impl Notice {
+    fn new(message: &str) -> Self {
+        Notice {
+            message: message.to_owned(),
+        }
+    }
+}
+
+fn notify(notice: Notice) {
+    if let Some(app) = APP.get() {
+        let _ = app.emit_all("notice", notice)
+            .inspect_err(|e| eprintln!("{e}"));
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,82 +1,143 @@
 const { invoke } = window.__TAURI__.tauri;
 const { listen } = window.__TAURI__.event;
 
-const roomIdList = document.querySelector('#room-ids');
-const roomName = document.querySelector('#room-id');
-const entranceDialog = document.querySelector('dialog#entrance');
-const roomIdInput = document.querySelector('input#room-id-input');
-const nameInput = document.querySelector('input#name-input');
-const enterButton = document.querySelector('button#enter');
-const messageContainer = document.querySelector('div#messages');
-const messageInput = document.querySelector('input#message');
-const sendButton = document.querySelector('button#send');
+const roomName = document.getElementById('room-id');
+
+const entranceDialog = new class {
+  /** @type {HTMLDialogElement} */
+  #dialog = document.getElementById('entrance');
+  #server = document.getElementById('server-input');
+  #room = document.getElementById('room-id-input');
+  #roomIds = document.getElementById('room-ids');
+  #name = document.getElementById('name-input');
+  #enter = document.getElementById('enter');
+
+  constructor() {
+    this.#name.onkeyup = _ => {
+      if (this.#name.value.trim().length) {
+        this.#enter.disabled = false;
+      } else {
+        this.#enter.disabled = true;
+      }
+    };
+
+    this.#enter.onclick = async e => {
+      const serverIp = this.#server.value.trim() || this.#server.placeholder;
+      const roomId = this.#room.value.trim() || this.#room.placeholder;
+      userName = this.#name.value.trim();
+      if (!this.#name.checkValidity()) {
+        this.#name.reportValidity();
+        return;
+      }
+      await enterRoom(serverIp, roomId, userName);
+      roomName.textContent = `${serverIp} / ${roomId}`;
+      this.close();
+    };
+  }
+
+  show() {
+    this.#dialog.showModal();
+  }
+
+  close() {
+    this.#dialog.close();
+  }
+
+  setRoomIds(ids) {
+    for (const id of ids) {
+      this.#roomIds.insertAdjacentHTML('beforeend', `<option>${id}</option>`);
+    }
+  }
+}();
+
+const messageContainer = new class {
+  #container = document.getElementById('messages');
+
+  append(message) {
+    const date = new Date();
+    const datetime = `${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+    const html = this.#createElement(message.name, message.message, datetime);
+    this.#container.insertAdjacentHTML('afterbegin', html);
+    this.#purge();
+  }
+
+  #purge() {
+    const messages = Array.from(this.#container.getElementsByClassName('message'));
+    if (messages.length >= 100) {
+      messages.slice(100).forEach(el => el.remove());
+    }
+  }
+
+  #createElement(name, message, datetime) {
+    return `<div class="message">
+      <div class="message-head">
+        <label class="message-name">${name}</label>
+        <div class="message-time">${datetime}</div>
+      </div>
+      <div class="message-body">${message}</div>
+    </div>`;
+  }
+}();
+
+const messageInput = new class {
+  /** @type {HTMLInputElement} */
+  #input = document.getElementById('message');
+  #send = document.getElementById('send');
+
+  constructor() {
+    this.#send.onclick = e => {
+      const message = this.#input.value.trim();
+      if (message.length > 0) {
+        sendMessage(message);
+        this.#input.value = '';
+      }
+    };
+  }
+}();
+
+const noticeDialog = new class {
+  /** @type {HTMLDialogElement} */
+  #dialog = document.getElementById('notice');
+  #message = document.getElementById('notice-message');
+  #close = document.getElementById('notice-close');
+
+  constructor() {
+    this.#close.onclick = _ => this.#dialog.close();
+  }
+
+  show(message) {
+    this.#message.textContent = message;
+    this.#dialog.showModal();
+  }
+}();
 
 let userName = '';
 
 async function sendMessage(message) {
-  await invoke('send_message', { 
+  await invoke('send_message', {
     name: userName,
     message
   });
   messageInput.value = '';
 }
 
-window.addEventListener('DOMContentLoaded', async () => {
-  await invoke('hello', {});
-
-  entranceDialog.show();
-  enterButton.onclick = async e => {
-    userName = nameInput.value.trim();
-    const roomId = roomIdInput.value.trim();
-
-    await enterRoom(roomId, userName);
-
-    roomName.textContent = roomId;
-    entranceDialog.hide();
-  };
-
-  sendButton.onclick = e => {
-    const message = messageInput.value.trim();
-    sendMessage(message);
-  };
-});
-
-async function enterRoom(room, name) {
+async function enterRoom(server, room, name) {
   invoke('enter_room', {
+    server,
     room,
     name
   });
 }
 
-listen('rooms', event => {
-  const roomIds = event.payload;
-  for (const roomId of roomIds) {
-    roomIdList.insertAdjacentHTML('beforeend', `<option value="${roomId}"></option>`);
-  }
-});
 
 listen('message', event => {
   const message = event.payload;
-  const date = new Date();
-  const datetime = `${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
-  const html = createMessageHTML(message.name, message.message, datetime);
-  messageContainer.insertAdjacentHTML('afterbegin', html);
-  purgeMessage();
+  messageContainer.append(message);
 });
 
-function createMessageHTML(name, message, datetime) {
-  return `<div class="message">
-    <div class="message-head">
-      <label class="message-name">${name}</label>
-      <div class="message-time">${datetime}</div>
-    </div>
-    <div class="message-body">${message}</div>
-  </div>`;
-}
+listen('notice', event => {
+  const { message } = event.payload;
+  noticeDialog.show(message);
+});
 
-function purgeMessage() {
-  const messages = Array.from(document.querySelectorAll('.message'));
-  if (messages.length >= 100) {
-    messages.slice(100).forEach(el => el.remove());
-  }
-}
+entranceDialog.show();
